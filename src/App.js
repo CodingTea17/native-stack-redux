51ba43ef0270ccce50d9cf026e611adf38a5e3af
Dawson M <dawson.mortenson@gmail.com>
2018-02-05 20:28:08 -0800

    create a library list component and connect it to the redux store

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
 import { Header } from './components/common';
-
+import LibraryList from './components';
 class App extends Component {
   render() {
     return (
       <Provider store={createStore(reducers)}>
         <View>
           <Header heading="Tech Stack"/>
+          <LibraryList />
         </View>
       </Provider>
     );
